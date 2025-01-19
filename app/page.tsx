b73ef132e64600f5a2eb9039ce87d56a7f8993f6
Chanuth Jayatissa <chanuthjayatissa@gmail.com>
2025-01-19 00:15:15 -0500

    finalized sign in page

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
                 Sign Up
               </button>
             </Link>
-            <Link href="/signin">
+            <Link href="/login/signin">
               <button className="px-6 py-2 text-lg font-semibold text-blue-500 border-2 border-blue-500 rounded-lg hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">
                 Sign In
               </button>
